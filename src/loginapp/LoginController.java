e7f72ada96cdbd925aee6c09c60226c174c4689c
Admin <Admin@DESKTOP-74S139L>
2025-02-20 10:33:45 +0700

    them code

--- src/loginapp/LoginController.java
+++ src/loginapp/LoginController.java
@@ -2,4 +2,17 @@ public class LoginController {
     private LoginForm view;
     private LoginModel model;
 
-    //code ở đây
+    public LoginController(LoginForm view) {
+        this.view = view;
+        this.model = new LoginModel();
+    }
+
+    public void handleLogin(String username, String password) {
+        if (model.validateUser(username, password)) {
+            view.showMessage("Đăng nhập thành công!");
+        } else {
+            view.showMessage("Sai tên đăng nhập hoặc mật khẩu.");
+        }
+    }
+}
+
